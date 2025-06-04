f51167ed5900b794a85bd3fb70fe5e0ea53a1476
SianMat <77469029+SianMat@users.noreply.github.com>
2025-06-04 12:45:56 +0100

    implement scroll to top

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import ScrollToTop from "../utils/ScrollToTop";
 
 export default function Root() {
   return (
     <>
+      <ScrollToTop />
       <Header />
       <main>
         <Outlet />
